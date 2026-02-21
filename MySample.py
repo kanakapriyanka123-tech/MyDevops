0aaf22075c7e16fb90ed7e42b5c3d1860ef885ff
kanakapriyanka123-tech <kanakapriyanka123@gmail.com>
2026-02-22 01:08:59 +0530

    Update MySample.py

--- MySample.py
+++ MySample.py
@@ -1,1 +1,23 @@
 print("Hello..Devops")
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+PORT = 8000
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"""
+        <html>
+            <head><title>My DevOps Page</title></head>
+            <body>
+                <h1>Hello from Jenkins CI 🚀</h1>
+                <p>This page is served using Python.</p>
+            </body>
+        </html>
+        """)
+
+with HTTPServer(("", PORT), MyHandler) as server:
+    print(f"Server running on port {PORT}")
+    server.serve_forever()
